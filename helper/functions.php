439b6982faa323143145d855a492aff2f51e84cd
sushaowei <sushaowei@chehang168.com>
2017-11-16 17:28:41 +0800

    xss

--- helper/functions.php
+++ helper/functions.php
@@ -4,3 +4,8 @@ function redirect($url){
 	header("Location:{$url}");
 	throw new \see\exception\NotFoundException("redirect", 301);
 }
+
+//过滤xss
+function xss_clean($str){
+	return \see\helper\Security::xss_clean($str);
+}
