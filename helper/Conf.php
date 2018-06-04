5ec8ef1733608a7b01c03ce78e4b8fc47eb039d5
sushaowei <sushaowei@chehang168.com>
2018-06-04 22:33:44 +0800

    conf set

--- helper/Conf.php
+++ helper/Conf.php
@@ -5,15 +5,14 @@
  * Date: 2018/5/30
  * Time: 下午4:58
  */
-
-namespace see\helper;
-
-
 class Conf
 {
     public static $conf;
     public static $init = false;
 
+    /**
+     * 初始化
+     */
     public static function initConf($file){
         if(self::$init ===false){
             if(file_exists($file)){
@@ -26,6 +25,9 @@ class Conf
         }
     }
 
+    /**
+     * 获取某个变量
+     */
     public static function get($key,$project,$environment){
         if(self::$init === false){
             trigger_error("Conf has not init",E_USER_ERROR);
@@ -35,5 +37,30 @@ class Conf
         }else{
             trigger_error("the key is not exists:{$key}",E_USER_ERROR);
         }
+    }
+
+    /**
+     * 获取项目所有变量
+     */
+    public static function getProject($project,$environment){
+        if(self::$init === false){
+            trigger_error("Conf has not init",E_USER_ERROR);
+        }
+
+        if(isset(self::$conf[$project])){
+            $result = [];
+            $params = self::$conf[$project];
+            if(is_array($params)){
+                foreach($params as $k=>$e){
+                    if(isset($params[$k][$environment])){
+                        $result[$k] = $params[$k][$environment];
+                    }
+                }
+            }
+            return $result;
+        }else{
+            trigger_error("the project is not exists:{$project}",E_USER_ERROR);
+        }
+
     }
 }
